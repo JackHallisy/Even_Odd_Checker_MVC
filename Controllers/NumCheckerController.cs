4f16513f2083c7e3a1fd42dde5d1ded8fdcc2339
JackHallisy <39491444+JackHallisy@users.noreply.github.com>
2018-10-26 17:11:08 -0400

    Add files via upload

---
+++ Controllers/NumCheckerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvenOddCheckerMVC.Controllers
+{
+    public class NumCheckerController : Controller
+    {
+        // GET: NumChecker
+        public ActionResult Index()
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 100);
+
+            ViewBag.randomNumber = randomNumber;
+
+            return View();
+        }
+
+    }
+}
